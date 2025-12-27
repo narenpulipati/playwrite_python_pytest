6b2584bfe704b5c55d42f14ae53a47a6b8d72d18
narenpulipati <pulipatinarendraa@gmail.com>
2025-12-27 18:11:56 +0530

    update mouse hover tests

---
+++ test_mouse_actions.py
@@ -0,0 +1,34 @@
+import pytest
+from playwright.sync_api import Page,expect
+
+#mouse hover actions
+def test_mouse_actions(page:Page):
+    page.goto("https://testautomationpractice.blogspot.com/")
+    page.locator(".dropbtn").hover()
+    page.locator(".dropdown-content a:nth-child(2)").hover()
+
+#right click functionality
+
+def test_mouse_right_click(page:Page):
+    page.goto("https://swisnl.github.io/jQuery-contextMenu/demo.html")
+    right_click=page.locator(".context-menu-one")
+    right_click.click(button="right")
+
+
+#double click functionality
+
+def test_mouse_double_click(page:Page):
+    page.goto("https://testautomationpractice.blogspot.com/")
+    button=page.get_by_text("Copy Text")
+    button.dblclick()
+    text=page.locator("id=field2")
+    expect(text).to_have_value("Hello World!")
+
+#drag and drop the elements
+def test_drag_drop(page:Page):
+    page.goto("https://testautomationpractice.blogspot.com/")
+    element1=page.locator("id=draggable")
+    element2=page.locator("id=droppable")
+    element1.drag_to(element2)
+    
+
